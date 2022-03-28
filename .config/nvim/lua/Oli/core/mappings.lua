f857854dc23cc59ec1d9e987c11e117754277dc6
Oli Morris <olimorris@users.noreply.github.com>
2022-03-28 21:21:35 +0100

    chore: neovim formatting

--- .config/nvim/lua/Oli/core/mappings.lua
+++ .config/nvim/lua/Oli/core/mappings.lua
@@ -440,7 +440,7 @@ M.completion_keymaps = function()
 
   local function has_words_before()
     local line, col = vim.api.nvim_win_get_cursor(0)
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
   end
 
   return {
