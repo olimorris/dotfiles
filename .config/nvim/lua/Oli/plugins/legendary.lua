1ed212c00991a9ecacbb8efd974828cb56e1ad36
Oli Morris <olimorris@users.noreply.github.com>
2022-05-12 16:57:55 +0100

    fix(neovim): prevent duplicates in legendary.nvim

--- .config/nvim/lua/Oli/plugins/legendary.lua
+++ .config/nvim/lua/Oli/plugins/legendary.lua
@@ -6,16 +6,20 @@ function M.setup()
     return
   end
 
-  legendary.setup({
-    include_builtin = false,
-    keymaps = require(config_namespace .. ".core.mappings").default_keymaps(),
-    commands = require(config_namespace .. ".core.commands").default_commands(),
-    autocmds = require(config_namespace .. ".core.autocmds").default_autocmds(),
-    auto_register_which_key = false,
-  })
+  if not vim.g.legendary then
+    legendary.setup({
+      include_builtin = false,
+      keymaps = require(config_namespace .. ".core.mappings").default_keymaps(),
+      commands = require(config_namespace .. ".core.commands").default_commands(),
+      autocmds = require(config_namespace .. ".core.autocmds").default_autocmds(),
+      auto_register_which_key = false,
+    })
 
-  legendary.bind_keymaps(require(config_namespace .. ".core.mappings").plugin_keymaps())
-  legendary.bind_keymaps(require(config_namespace .. ".core.commands").plugin_commands())
+    legendary.bind_keymaps(require(config_namespace .. ".core.mappings").plugin_keymaps())
+    legendary.bind_keymaps(require(config_namespace .. ".core.commands").plugin_commands())
+  end
+
+  vim.g.legendary = true
 end
 
 return M
