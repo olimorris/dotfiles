9ad2c87526cdd91f7136416e96500e0b058c7e10
Oli Morris <olimorris@users.noreply.github.com>
2022-09-01 17:44:06 +0100

    chore(fish): updates to aliases and fzf config

--- .config/fish/fzf.fish
+++ .config/fish/fzf.fish
@@ -8,6 +8,20 @@ set -a FZF_DEFAULT_OPTS --history=$HOME/.fzf_history
 set -a FZF_DEFAULT_OPTS --bind=ctrl-t:top
 set -a FZF_DEFAULT_OPTS --pointer='▶' --marker='✓'
 
-# FZF theming is in the conf.d/update_theme.fish file
+if [ "$macOS_Theme" = light ]
+    set FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS --color bg:'#fafafa',bg+:'#fafafa',fg:'#6a6a6a'
+    set FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS --color fg+:'#9a77cf',hl:'#1da912',hl+:'#1da912'
+    set FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS --color preview-bg:'#fafafa',preview-fg:'#6a6a6a'
+    set FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS --color border:'#6a6a6a',gutter:'#fafafa',header:'#1da912'
+    set FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS --color info:'#1da912',marker:'#e05661',pointer:'#9a77cf'
+    set FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS --color prompt:'#eea825',spinner:'#118dc3'
+else if [ "$macOS_Theme" = dark ]
+    set FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS --color bg:'#282c34',bg+:'#282c34',fg:'#abb2bf'
+    set FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS --color fg+:'#c678dd',hl:'#98c379',hl+:'#98c379'
+    set FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS --color preview-bg:'#282c34',preview-fg:'#abb2bf'
+    set FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS --color border:'#abb2bf',gutter:'#282c34',header:'#98c379'
+    set FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS --color info:'#98c379',marker:'#e06c7f',pointer:'#c678dd'
+    set FZF_DEFAULT_OPTS $FZF_DEFAULT_OPTS --color prompt:'#e5c07b',spinner:'#61afef'
+end
 
 set fzf_fd_opts --hidden --no-ignore --exclude=.git --exclude=Library
