d92e2f79c6d0a4e3c211f44316b4c0b88404a207
Oli Morris <olimorris@users.noreply.github.com>
2022-09-01 17:44:34 +0100

    chore(neovim): update null-ls config

--- .config/nvim/lua/Oli/plugins/null-ls.lua
+++ .config/nvim/lua/Oli/plugins/null-ls.lua
@@ -3,27 +3,32 @@ if not ok then
   return
 end
 
+local b = null_ls.builtins
+
 local sources = {
   -- Code completion
-  null_ls.builtins.code_actions.eslint_d,
+  b.code_actions.eslint_d,
 
   -- Diagnostics
-  null_ls.builtins.diagnostics.jsonlint,
+  b.diagnostics.eslint_d,
+  b.diagnostics.jsonlint,
 
   -- Formatting
-  null_ls.builtins.formatting.stylua,
-  null_ls.builtins.formatting.fixjson,
-  null_ls.builtins.formatting.phpcsfixer,
-  null_ls.builtins.formatting.rubocop,
-  null_ls.builtins.formatting.shfmt.with({
-    filetypes = { "sh", "zsh" },
-  }),
-  null_ls.builtins.formatting.prettier.with({
+  b.formatting.fish_indent,
+  b.formatting.fixjson,
+  b.formatting.phpcsfixer,
+  b.formatting.prettier.with({
     filetypes = { "css", "dockerfile", "html", "javascript", "markdown", "vue", "yaml" },
   }),
+  b.formatting.rubocop,
+  b.formatting.shfmt.with({
+    filetypes = { "sh", "zsh" },
+  }),
+  b.formatting.stylua,
 }
 
 null_ls.setup({
   sources = sources,
   on_attach = om.lsp.on_attach,
+  update_on_insert = true,
 })
