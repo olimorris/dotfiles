d5a80a4ab8c84d5a03b9ef6084b30374f1c2ed75
Oli Morris <olimorris@users.noreply.github.com>
2022-10-28 17:05:02 +0100

    chore(neovim): neotest config

--- .config/nvim/lua/Oli/plugins/neotest.lua
+++ .config/nvim/lua/Oli/plugins/neotest.lua
@@ -1,48 +1,54 @@
 local ok, neotest = om.safe_require("neotest")
 if not ok then return end
 
-neotest.setup({
-  adapters = {
-    require("neotest-plenary"),
-    require("neotest-rspec"),
-    require("neotest-phpunit"),
-  },
-  consumers = {
-    overseer = require("neotest.consumers.overseer"),
-  },
-  diagnostic = {
-    enabled = false,
-  },
-  log_level = 1,
-  icons = {
-    expanded = "",
-    child_prefix = "",
-    child_indent = "",
-    final_child_prefix = "",
-    non_collapsible = "",
-    collapsed = "",
+local M = {}
 
-    passed = "",
-    running = "",
-    failed = "",
-    unknown = "",
-    skipped = "",
-  },
-  floating = {
-    border = "single",
-    max_height = 0.8,
-    max_width = 0.9,
-  },
-  summary = {
-    mappings = {
-      attach = "a",
-      expand = { "<CR>", "<2-LeftMouse>" },
-      expand_all = "e",
-      jumpto = "i",
-      output = "o",
-      run = "r",
-      short = "O",
-      stop = "u",
+function M.setup()
+  return neotest.setup({
+    adapters = {
+      require("neotest-plenary"),
+      require("neotest-rspec"),
+      require("neotest-phpunit"),
     },
-  },
-})
+    consumers = {
+      overseer = require("neotest.consumers.overseer"),
+    },
+    diagnostic = {
+      enabled = false,
+    },
+    log_level = 1,
+    icons = {
+      expanded = "",
+      child_prefix = "",
+      child_indent = "",
+      final_child_prefix = "",
+      non_collapsible = "",
+      collapsed = "",
+
+      passed = "",
+      running = "",
+      failed = "",
+      unknown = "",
+      skipped = "",
+    },
+    floating = {
+      border = "single",
+      max_height = 0.8,
+      max_width = 0.9,
+    },
+    summary = {
+      mappings = {
+        attach = "a",
+        expand = { "<CR>", "<2-LeftMouse>" },
+        expand_all = "e",
+        jumpto = "i",
+        output = "o",
+        run = "r",
+        short = "O",
+        stop = "u",
+      },
+    },
+  })
+end
+
+return M
