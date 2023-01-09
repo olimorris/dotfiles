af9977e38ce40d8f94937da94d571808d3415b98
Oli Morris <olimorris@users.noreply.github.com>
2023-01-09 23:20:02 +0000

    fix(neovim): chatgpt plugin

--- .config/nvim/lua/Oli/plugins/coding.lua
+++ .config/nvim/lua/Oli/plugins/coding.lua
@@ -97,7 +97,7 @@ return {
         },
       })
     end,
-    config = {
+    opts = {
       welcome_message = "",
       answer_sign = "",
       question_sign = "",
@@ -130,70 +130,70 @@ return {
         cycle_windows = "<Tab>",
       },
     },
-    {
-      "zbirenbaum/copilot.lua", -- AI programming
-      event = "InsertEnter",
-      init = function()
-        require("legendary").keymaps({
-          {
-            itemgroup = "Copilot",
-            description = "AI programming",
-            icon = "",
-            keymaps = {
-              {
-                "<C-a>",
-                function() require("copilot.suggestion").accept() end,
-                description = "Accept suggestion",
-                mode = { "i" },
-                opts = { silent = true },
-              },
-              {
-                "<C-x>",
-                function() require("copilot.suggestion").dismiss() end,
-                description = "Dismiss suggestion",
-                mode = { "i" },
-                opts = { silent = true },
-              },
-              {
-                "<C-n>",
-                function() require("copilot.suggestion").next() end,
-                description = "Next suggestion",
-                mode = { "i" },
-                opts = { silent = true },
-              },
-              {
-                "<C-\\>",
-                function() require("copilot.panel").open() end,
-                description = "Show Copilot panel",
-                mode = { "n", "i" },
-              },
+  },
+  {
+    "zbirenbaum/copilot.lua", -- AI programming
+    event = "InsertEnter",
+    init = function()
+      require("legendary").keymaps({
+        {
+          itemgroup = "Copilot",
+          description = "AI programming",
+          icon = "",
+          keymaps = {
+            {
+              "<C-a>",
+              function() require("copilot.suggestion").accept() end,
+              description = "Accept suggestion",
+              mode = { "i" },
+              opts = { silent = true },
+            },
+            {
+              "<C-x>",
+              function() require("copilot.suggestion").dismiss() end,
+              description = "Dismiss suggestion",
+              mode = { "i" },
+              opts = { silent = true },
+            },
+            {
+              "<C-n>",
+              function() require("copilot.suggestion").next() end,
+              description = "Next suggestion",
+              mode = { "i" },
+              opts = { silent = true },
+            },
+            {
+              "<C-\\>",
+              function() require("copilot.panel").open() end,
+              description = "Show Copilot panel",
+              mode = { "n", "i" },
             },
           },
-        })
-
-        require("legendary").commands({
-          {
-            itemgroup = "Copilot",
-            commands = {
-              {
-                ":CopilotToggle",
-                function() require("copilot.suggestion").toggle_auto_trigger() end,
-                description = "Toggle on/off for buffer",
-              },
-            },
-          },
-        })
-      end,
-      opts = {
-        panel = {
-          auto_refresh = true,
         },
-        suggestion = {
-          auto_trigger = true, -- Suggest as we start typing
-          keymap = {
-            accept_word = "<C-l>",
-            accept_line = "<C-j>",
+      })
+
+      require("legendary").commands({
+        {
+          itemgroup = "Copilot",
+          commands = {
+            {
+              ":CopilotToggle",
+              function() require("copilot.suggestion").toggle_auto_trigger() end,
+              description = "Toggle on/off for buffer",
+            },
           },
+        },
+      })
+    end,
+    opts = {
+      panel = {
+        auto_refresh = true,
+      },
+      suggestion = {
+        auto_trigger = true, -- Suggest as we start typing
+        keymap = {
+          accept_word = "<C-l>",
+          accept_line = "<C-j>",
         },
       },
     },
