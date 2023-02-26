a50592b2399dbcb3ea0c39ee8176f58e72460645
Oli Morris <olimorris@users.noreply.github.com>
2023-02-26 11:31:44 +0000

    chore(neovim): add `overseer.nvim` mapping

--- .config/nvim/lua/Oli/plugins/coding.lua
+++ .config/nvim/lua/Oli/plugins/coding.lua
@@ -356,6 +356,16 @@ return {
   {
     "stevearc/overseer.nvim", -- Task runner and job management
     lazy = true,
+    opts = {
+      component_aliases = {
+        default_neotest = {
+          "on_output_summarize",
+          "on_exit_set_status",
+          "on_complete_dispose",
+        },
+      },
+      templates = { "builtin", "java_build" },
+    },
     init = function()
       require("legendary").commands({
         {
@@ -378,17 +388,25 @@ return {
           },
         },
       })
-    end,
-    opts = {
-      component_aliases = {
-        default_neotest = {
-          "on_output_summarize",
-          "on_exit_set_status",
-          "on_complete_dispose",
+      require("legendary").keymaps({
+        itemgroup = "Overseer",
+        keymaps = {
+          {
+            "<Leader>o",
+            function()
+              local overseer = require("overseer")
+              local tasks = overseer.list_tasks({ recent_first = true })
+              if vim.tbl_isempty(tasks) then
+                vim.notify("No tasks found", vim.log.levels.WARN)
+              else
+                overseer.run_action(tasks[1], "restart")
+              end
+            end,
+            description = "Run the last Overseer task",
+          },
         },
-      },
-      templates = { "builtin", "java_build" },
-    },
+      })
+    end,
   },
   {
     "nvim-neotest/neotest",
