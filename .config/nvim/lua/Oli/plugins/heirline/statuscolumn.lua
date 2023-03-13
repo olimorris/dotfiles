6a2d8c3eb7d44b6f125e0c70bb55b0677611a77f
Oli Morris <olimorris@users.noreply.github.com>
2023-03-13 20:56:54 +0000

    fix(neovim): statuscolumn and virtual text

--- .config/nvim/lua/Oli/plugins/heirline/statuscolumn.lua
+++ .config/nvim/lua/Oli/plugins/heirline/statuscolumn.lua
@@ -104,6 +104,7 @@ M.line_numbers = {
 }
 
 M.folds = {
+  condition = function() return vim.v.virtnum == 0 end,
   init = function(self)
     self.lnum = vim.v.lnum
     self.folded = vim.fn.foldlevel(self.lnum) > vim.fn.foldlevel(self.lnum - 1)
@@ -135,32 +136,39 @@ M.folds = {
 }
 
 M.git_signs = {
-  condition = function() return conditions.is_git_repo() end,
-  init = function(self)
-    local signs = vim.fn.sign_getplaced(vim.api.nvim_get_current_buf(), {
-      group = "gitsigns_vimfn_signs_",
-      id = vim.v.lnum,
-      lnum = vim.v.lnum,
-    })
+  {
+    condition = function() return not conditions.is_git_repo() or vim.v.virtnum ~= 0 end,
+    provider = "┃",
+    hl = "HeirlineStatusColumn",
+  },
+  {
+    condition = function() return conditions.is_git_repo() and vim.v.virtnum == 0 end,
+    init = function(self)
+      local signs = vim.fn.sign_getplaced(vim.api.nvim_get_current_buf(), {
+        group = "gitsigns_vimfn_signs_",
+        id = vim.v.lnum,
+        lnum = vim.v.lnum,
+      })
 
-    if #signs == 0 or signs[1].signs == nil or #signs[1].signs == 0 or signs[1].signs[1].name == nil then
-      self.sign = nil
-    else
-      self.sign = signs[1].signs[1]
-    end
+      if #signs == 0 or signs[1].signs == nil or #signs[1].signs == 0 or signs[1].signs[1].name == nil then
+        self.sign = nil
+      else
+        self.sign = signs[1].signs[1]
+      end
 
-    self.has_sign = self.sign ~= nil
-  end,
-  provider = "┃",
-  hl = function(self)
-    if self.has_sign then return self.sign.name end
-    return "HeirlineStatusColumn"
-  end,
-  on_click = {
-    name = "gitsigns_click",
-    callback = function(self, ...)
-      if self.handlers.git_signs then self.handlers.git_signs(self.click_args(self, ...)) end
+      self.has_sign = self.sign ~= nil
     end,
+    provider = "┃",
+    hl = function(self)
+      if self.has_sign then return self.sign.name end
+      return "HeirlineStatusColumn"
+    end,
+    on_click = {
+      name = "gitsigns_click",
+      callback = function(self, ...)
+        if self.handlers.git_signs then self.handlers.git_signs(self.click_args(self, ...)) end
+      end,
+    },
   },
 }
 
