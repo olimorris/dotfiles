09289beec40211f71201cce6fc78bc18eccd7653
Oli Morris <olimorris@users.noreply.github.com>
2023-05-08 21:56:27 +0100

    feat: auto add quotes in vue and html elements
    Source: https://medium.com/scoro-engineering/5-smart-mini-snippets-for-making-text-editing-more-fun-in-neovim-b55ffb96325a

--- .config/nvim/lua/Oli/config/autocmds.lua
+++ .config/nvim/lua/Oli/config/autocmds.lua
@@ -139,6 +139,31 @@ return {
       end,
       opts = { pattern = "json" },
     },
+    {
+      "FileType",
+      function()
+        vim.keymap.set("i", "=", function()
+          -- The cursor location does not give us the correct node in this case, so we
+          -- need to get the node to the left of the cursor
+          local cursor = vim.api.nvim_win_get_cursor(0)
+          local left_of_cursor_range = { cursor[1] - 1, cursor[2] - 1 }
+
+          local node = vim.treesitter.get_node({ pos = left_of_cursor_range })
+          local nodes_active_in = {
+            "attribute_name",
+            "directive_argument",
+            "directive_name",
+          }
+          if not node or not vim.tbl_contains(nodes_active_in, node:type()) then
+            -- The cursor is not on an attribute node
+            return "="
+          end
+
+          return '=""<left>'
+        end, { expr = true, buffer = true })
+      end,
+      opts = { pattern = { "html", "vue" } },
+    },
   },
   {
     name = "QuickfixFormatting",
