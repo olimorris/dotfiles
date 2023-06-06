ddc061c9fc5c1b5f716cf289b2115cfeff479c9e
Oli Morris <olimorris@users.noreply.github.com>
2023-06-06 21:15:38 +0100

    chore(neovim): move utils and add float_term

--- .config/nvim/lua/Oli/util/init.lua
+++ .config/nvim/lua/Oli/util/init.lua
@@ -8,6 +8,33 @@ function om.has(feature) return vim.fn.has(feature) > 0 end
 
 om.nightly = om.has("nvim-0.10")
 
+local terminals = {}
+
+-- Opens a floating terminal (interactive by default)
+function om.float_term(cmd, opts)
+  opts = vim.tbl_deep_extend("force", {
+    ft = "lazyterm",
+    size = { width = 0.9, height = 0.9 },
+  }, opts or {}, { persistent = true })
+
+  local termkey = vim.inspect({ cmd = cmd or "shell", cwd = opts.cwd, env = opts.env })
+
+  if terminals[termkey] and terminals[termkey]:buf_valid() then
+    terminals[termkey]:toggle()
+  else
+    terminals[termkey] = require("lazy.util").float_term(cmd, opts)
+    local buf = terminals[termkey].buf
+    vim.b[buf].lazyterm_cmd = cmd
+    if opts.esc_esc == false then vim.keymap.set("t", "<esc>", "<esc>", { buffer = buf, nowait = true }) end
+    vim.api.nvim_create_autocmd("BufEnter", {
+      buffer = buf,
+      callback = function() vim.cmd.startinsert() end,
+    })
+  end
+
+  return terminals[termkey]
+end
+
 ---Source a lua or vimscript file
 ---@param path string path relative to the nvim directory
 ---@param prefix boolean?
