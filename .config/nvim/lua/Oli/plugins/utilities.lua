42c9a81ed283047b27908fa9e64a660173417aec
Oli Morris <olimorris@users.noreply.github.com>
2023-06-06 21:16:27 +0100

    chore(neovim): remove `toggleterm.nvim`

--- .config/nvim/lua/Oli/plugins/utilities.lua
+++ .config/nvim/lua/Oli/plugins/utilities.lua
@@ -27,36 +27,6 @@ return {
     end,
   },
   {
-    "akinsho/nvim-toggleterm.lua", -- Easily toggle and position the terminal
-    cmd = "ToggleTerm",
-    init = function()
-      require("legendary").keymaps({
-        { "<C-x>", "<cmd>ToggleTerm<CR>", description = "Toggleterm", mode = { "n", "t" } },
-      })
-    end,
-    opts = {
-      direction = "float",
-      float_opts = {
-        border = "single",
-        height = function() return math.floor(0.9 * vim.fn.winheight("%")) end,
-        -- width = function()
-        --   return math.floor(0.9 * vim.fn.winwidth("%"))
-        -- end,
-        highlights = {
-          background = "ToggleTerm",
-          border = "ToggleTermBorder",
-        },
-      },
-      -- direction = "horizontal",
-      -- size = 8,
-      -- shade_terminals = true,
-      shading_factor = 3, -- Match our background
-      hide_numbers = true,
-      close_on_exit = true,
-      start_in_insert = true,
-    },
-  },
-  {
     "olimorris/persisted.nvim", -- Session management
     priority = 100,
     opts = {
@@ -69,7 +39,7 @@ return {
           "alpha",
           "oil",
           "lazy",
-          ""
+          "",
         }
 
         for _, filetype in ipairs(excluded_filetypes) do
