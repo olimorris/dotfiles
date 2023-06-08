bb74b5219e10e5ebb9988b5d4340c5c9cbbff9fb
Oli Morris <olimorris@users.noreply.github.com>
2023-06-08 23:36:49 +0100

    fix(neovim): big screen lines

--- .config/nvim/lua/Oli/util/init.lua
+++ .config/nvim/lua/Oli/util/init.lua
@@ -36,7 +36,7 @@ function om.float_term(cmd, opts)
 end
 
 function om.on_big_screen()
-  return vim.o.columns > 150 and vim.o.lines > 40
+  return vim.o.columns > 150 and vim.o.lines >= 40
 end
 
 ---Source a lua or vimscript file
