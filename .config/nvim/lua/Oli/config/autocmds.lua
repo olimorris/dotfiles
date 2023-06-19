eadd712ec0ed74023c9947a86001534ce00128ec
Oli Morris <olimorris@users.noreply.github.com>
2023-06-18 22:06:54 -0700

    fix(neovim): conceal tailwind attributes

--- .config/nvim/lua/Oli/config/autocmds.lua
+++ .config/nvim/lua/Oli/config/autocmds.lua
@@ -9,7 +9,31 @@ return {
       function()
         vim.opt.conceallevel = 2 -- Concealed text is completely hidden
         local bufnr = vim.api.nvim_get_current_buf()
-        om.ConcealHTML(bufnr)
+
+        ---Conceal HTML class attributes. Ideal for big TailwindCSS class lists
+        ---Ref: https://gist.github.com/mactep/430449fd4f6365474bfa15df5c02d27b
+        local conceal_ns = vim.api.nvim_create_namespace("class_conceal")
+        local language_tree = vim.treesitter.get_parser(bufnr, "html")
+        local syntax_tree = language_tree:parse()
+        local root = syntax_tree[1]:root()
+
+        local query = vim.treesitter.query.parse(
+          "html",
+          [[
+    ((attribute
+        (attribute_name) @att_name (#eq? @att_name "class")
+        (quoted_attribute_value (attribute_value) @class_value) (#set! @class_value conceal "…")))
+    ]]
+        )
+
+        for _, captures, metadata in query:iter_matches(root, bufnr, root:start(), root:end_(), {}) do
+          local start_row, start_col, end_row, end_col = captures[2]:range()
+          vim.api.nvim_buf_set_extmark(bufnr, conceal_ns, start_row, start_col, {
+            end_line = end_row,
+            end_col = end_col,
+            conceal = metadata[2].conceal,
+          })
+        end
       end,
       opts = {
         pattern = { "*.html" },
--- .config/nvim/lua/Oli/config/functions.lua
+++ .config/nvim/lua/Oli/config/functions.lua
@@ -6,34 +6,6 @@ function om.ChangeFiletype()
   end)
 end
 
-local conceal_ns = vim.api.nvim_create_namespace("class_conceal")
-
----Conceal HTML class attributes. Ideal for big TailwindCSS class lists
----Ref: https://gist.github.com/mactep/430449fd4f6365474bfa15df5c02d27b
-function om.ConcealHTML(bufnr)
-  local language_tree = vim.treesitter.get_parser(bufnr, "html")
-  local syntax_tree = language_tree:parse()
-  local root = syntax_tree[1]:root()
-
-  local query = vim.treesitter.query.parse_query(
-    "html",
-    [[
-    ((attribute
-        (attribute_name) @att_name (#eq? @att_name "class")
-        (quoted_attribute_value (attribute_value) @class_value) (#set! @class_value conceal "…")))
-    ]]
-  )
-
-  for _, captures, metadata in query:iter_matches(root, bufnr, root:start(), root:end_()) do
-    local start_row, start_col, end_row, end_col = captures[2]:range()
-    vim.api.nvim_buf_set_extmark(bufnr, conceal_ns, start_row, start_col, {
-      end_line = end_row,
-      end_col = end_col,
-      conceal = metadata[2].conceal,
-    })
-  end
-end
-
 function om.ListBranches()
   local branches = vim.fn.systemlist([[git branch 2>/dev/null]])
   local new_branch_prompt = "Create new branch"
