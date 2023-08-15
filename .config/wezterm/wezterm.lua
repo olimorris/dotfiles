c8af1a1f4fcea91ba098463c49547b74ee1c741b
Oli Morris <olimorris@users.noreply.github.com>
2023-08-15 22:14:25 +0100

    wip(wezterm): tweak colors and config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,8 +3,11 @@ local config = {}
 
 if wezterm.config_builder then config = wezterm.config_builder() end
 
+config.hide_tab_bar_if_only_one_tab = true
+
 config.color_scheme = "onedarkpro_onedark"
-config.line_height = 1.5
+
+config.line_height = 1.6
 
 config.font_size = 21
 config.font = wezterm.font("Operator Mono", { weight = "Book" })
@@ -21,19 +24,12 @@ config.font_rules = {
     italic = true,
     font = wezterm.font("Operator Mono", { stretch = "Expanded", italic = true, weight = "Bold" }),
   },
-  {
-    intensity = "Normal",
-    italic = true,
-    font = wezterm.font("Operator Mono", { stretch = "Expanded", italic = true, weight = "Book" }),
-  },
 }
-
-config.hide_tab_bar_if_only_one_tab = true
 
 config.window_padding = {
   left = 5,
   right = 5,
-  top = 5,
+  top = 0,
   bottom = 0,
 }
 config.window_decorations = "RESIZE"
