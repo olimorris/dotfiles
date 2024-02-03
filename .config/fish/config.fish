1c2e0d21d137f6d66eda922aee25ecca5a67cab7
Oli Morris <olimorris@users.noreply.github.com>
2024-02-03 14:06:14 +0000

    feat: add postgres to mise

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,11 @@ set -x GOPATH "$HOME/Code/Go"
 set -x STARSHIP_CONFIG "$HOME/.config/starship/starship.toml"
 set -gx macOS_Theme (cat $HOME/.color_mode | string collect)
 
+# Add OpenSSL to PATH
+set -gx LDFLAGS "-L/opt/homebrew/Cellar/openssl@3/3.2.1/lib"
+set -gx CPPFLAGS "-I/opt/homebrew/Cellar/openssl@3/3.2.1/include"
+set -gx PKG_CONFIG_PATH "/opt/homebrew/Cellar/openssl@3/3.2.1/lib/pkgconfig"
+
 set fish_color_param cyan
 set fish_pager_color_completion blue --bold
 set fish_color_normal black
