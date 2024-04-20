b020ea8a8be1be24373410674d7b1e27561414bd
Oli Morris <olimorris@users.noreply.github.com>
2024-04-20 21:35:08 +0100

    refactor(neovim): replace vim.loop with vim.uv

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -3,7 +3,7 @@ require("config.options")
 
 -- Begin Lazy install and plugin setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   vim.fn.system({
     "git",
     "clone",
