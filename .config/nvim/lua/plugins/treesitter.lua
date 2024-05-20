abd0e9035a9b58cde79063b11dcc191d144775a9
Oli Morris <olimorris@users.noreply.github.com>
2024-05-20 22:57:52 +0100

    refactor(treesitter): trim config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -60,31 +60,8 @@ return {
         },
         indent = { enable = true },
 
-        -- nvim-ts-autotag plugin
-        autotag = { enable = true },
-
         -- nvim-treesitter-endwise plugin
         endwise = { enable = true },
-
-        -- playground
-        playground = {
-          enable = true,
-          disable = {},
-          updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-          persist_queries = false, -- Whether the query persists across vim sessions
-          keybindings = {
-            toggle_query_editor = "o",
-            toggle_hl_groups = "i",
-            toggle_injected_languages = "t",
-            toggle_anonymous_nodes = "a",
-            toggle_language_display = "I",
-            focus_language = "f",
-            unfocus_language = "F",
-            update = "R",
-            goto_node = "<cr>",
-            show_help = "?",
-          },
-        },
 
         textobjects = {
           select = {
