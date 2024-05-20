3f893f382ba7a4776e7e2ee7268e7103688c3ce9
Oli Morris <olimorris@users.noreply.github.com>
2024-05-20 22:59:07 +0100

    refactor(neovim): good bye comment.nvim

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -69,23 +69,6 @@ return {
     config = true,
   },
   {
-    "numToStr/Comment.nvim", -- Comment out lines with gcc
-    keys = { "gcc", { "gc", mode = "v" } },
-    init = function()
-      require("legendary").keymaps({
-        {
-          ":Comment",
-          {
-            n = "gcc",
-            v = "gc",
-          },
-          description = "Toggle comment",
-        },
-      })
-    end,
-    config = true,
-  },
-  {
     "ThePrimeagen/refactoring.nvim", -- Refactor code like Martin Fowler
     lazy = true,
     init = function()
