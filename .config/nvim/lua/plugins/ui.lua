2024d2366512ee5e95a3fca13a89ae83d97d3015
Oli Morris <olimorris@users.noreply.github.com>
2024-06-30 15:59:19 +0100

    fix(nvim): gitsigns highlights

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -84,12 +84,12 @@ return {
     "lewis6991/gitsigns.nvim", -- Git signs in the statuscolumn
     opts = {
       signs = {
-        add = { hl = "GitSignsAdd", text = "│" },
-        change = { hl = "GitSignsChange", text = "│" },
-        delete = { hl = "GitSignsDelete", text = "│" },
-        topdelete = { hl = "GitSignsDelete", text = "│" },
-        changedelete = { hl = "GitSignsChange", text = "│" },
-        untracked = { hl = "GitSignsUntracked", text = "│" },
+        add = { text = "│" },
+        change = { text = "│" },
+        delete = { text = "│" },
+        topdelete = { text = "│" },
+        changedelete = { text = "│" },
+        untracked = { text = "│" },
       },
       numhl = false,
       linehl = false,
