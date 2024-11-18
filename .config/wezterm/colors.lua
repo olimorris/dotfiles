dc3c53f4ad5cc25d75875f8e2f08917367d4b99b
Oli Morris <olimorris@users.noreply.github.com>
2024-11-18 18:07:42 +0000

    refactor(wezterm): update colors

--- .config/wezterm/colors.lua
+++ .config/wezterm/colors.lua
@@ -1,27 +1,15 @@
 local wezterm = require("wezterm")
 
----Fetch the correct color scheme
-local function get_color_scheme()
-  local file = io.open(wezterm.home_dir .. "/.color_mode", "rb")
-  if not file then
-    wezterm.log_info("Could not read color")
-    return "onedarkpro_onedark"
-  end
-  local color = file:read("*a")
-  file:close()
-
-  color = color:gsub("^%s*(.-)%s*$", "%1")
-  wezterm.log_info("Current color mode: " .. color)
-
-  if color == "dark" then
-    return "onedarkpro_onedark"
+local function scheme_for_appearance(appearance)
+  if appearance:find("Dark") then
+    return "onedarkpro_vaporwave"
   end
   return "onedarkpro_onelight"
 end
 
 local function colors(config)
   config.color_scheme_dirs = { wezterm.home_dir .. "/.cache/nvim/onedarkpro_dotfiles/extras/wezterm" }
-  config.color_scheme = get_color_scheme()
+  config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 end
 
 return colors
