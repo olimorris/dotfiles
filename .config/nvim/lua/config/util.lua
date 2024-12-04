f610a28d276c964265be95bf55b4e36311da483d
Oli Morris <oliver.morris@automationanywhere.com>
2024-12-04 15:29:12 +0000

    feat(neovim): global var for personal mac

--- .config/nvim/lua/config/util.lua
+++ .config/nvim/lua/config/util.lua
@@ -1,14 +1,15 @@
 -- Set the global namespace
 _G.om = {}
 
+om.nightly = om.has("nvim-0.11")
+om.on_personal = vim.fn.getenv("USER") == "Oli"
+
 ---Check if a certain feature/version/commit exists in nvim
 ---@param feature string
 ---@return boolean
 function om.has(feature)
   return vim.fn.has(feature) > 0
 end
-
-om.nightly = om.has("nvim-0.11")
 
 ---Determine if you're on an external monitor
 ---@return boolean
