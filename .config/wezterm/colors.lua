066a381ce30dbf34d6b24af6adef013c8665a27f
Oli Morris <olimorris@users.noreply.github.com>
2024-12-18 08:34:16 +0000

    refactor(wezterm): colors

--- .config/wezterm/colors.lua
+++ .config/wezterm/colors.lua
@@ -1,6 +1,7 @@
 local wezterm = require("wezterm")
 
-local function scheme_for_appearance(appearance)
+local function scheme_for_appearance()
+  local appearance = wezterm.gui.get_appearance()
   if appearance:find("Dark") then
     return "onedarkpro_vaporwave"
   end
@@ -9,7 +10,7 @@ end
 
 local function colors(config)
   config.color_scheme_dirs = { wezterm.home_dir .. "/.cache/nvim/onedarkpro_dotfiles/extras/wezterm" }
-  config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+  config.color_scheme = scheme_for_appearance()
 end
 
 return colors
