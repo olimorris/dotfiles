02cf469b33f74221398f0e4fe47555c0164c1a24
Oli Morris <olimorris@users.noreply.github.com>
2025-03-12 21:16:43 +0000

    feat(neovim): change default codecompanion model

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -18,7 +18,7 @@ return {
           copilot = require("codecompanion.adapters").extend("copilot", {
             schema = {
               model = {
-                default = "claude-3.5-sonnet",
+                default = "claude-3.7-sonnet",
               },
             },
           }),
