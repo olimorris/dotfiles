318b78c8e838095d7b3f7f562445b2edc25c795f
Oli Morris <olimorris@users.noreply.github.com>
2025-04-30 23:39:29 +0100

    feat(neovim): switch to gemini-2.5 in CodeCompanion

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -41,7 +41,7 @@ return {
           return require("codecompanion.adapters").extend("copilot", {
             schema = {
               model = {
-                default = "claude-3.7-sonnet",
+                default = "gemini-2.5-pro",
               },
             },
           })
