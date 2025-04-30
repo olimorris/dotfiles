5da4b4aa1d65f7be607313c117b3a241e8ca1d8b
Oli Morris <olimorris@users.noreply.github.com>
2025-04-30 23:39:16 +0100

    refactor(neovim): update CodeCompanion config

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -119,37 +119,6 @@ return {
         end,
       },
       prompt_library = {
-        ["Docusaurus"] = {
-          strategy = "chat",
-          description = "Write documentation for me",
-          opts = {
-            index = 11,
-            is_slash_cmd = false,
-            auto_submit = false,
-            short_name = "docs",
-          },
-          references = {
-            {
-              type = "file",
-              path = {
-                "doc/.vitepress/config.mjs",
-                "lua/codecompanion/config.lua",
-                "README.md",
-              },
-            },
-          },
-          prompts = {
-            {
-              role = "user",
-              content = [[I'm rewriting the documentation for my plugin CodeCompanion.nvim, as I'm moving to a vitepress website. Can you help me rewrite it?
-
-I'm sharing my vitepress config file so you have the context of how the documentation website is structured in the `sidebar` section of that file.
-
-I'm also sharing my `config.lua` file which I'm mapping to the `configuration` section of the sidebar.
-]],
-            },
-          },
-        },
         ["Test workflow"] = {
           strategy = "workflow",
           description = "Use a workflow to test the plugin",
@@ -290,6 +259,10 @@ I'm also sharing my `config.lua` file which I'm mapping to the `configuration` s
               callback = function()
                 return require("vectorcode.integrations").codecompanion.chat.make_tool()
               end,
+            },
+            opts = {
+              auto_submit_success = true,
+              auto_submit_errors = false,
             },
           },
         },
