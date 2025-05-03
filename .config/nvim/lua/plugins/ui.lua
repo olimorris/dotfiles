2275dd9debf653c688f85021c06605a08abf842a
Oli Morris <olimorris@users.noreply.github.com>
2025-05-03 22:52:06 +0100

    feat(neovim): add awesome csvview.nvim

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -275,4 +275,50 @@ return {
       })
     end,
   },
+  {
+    "hat0uma/csvview.nvim", -- Make CSV files great again
+    opts = {
+      parser = { comments = { "#", "//" } },
+      keymaps = {
+        -- Text objects for selecting fields
+        textobject_field_inner = { "if", mode = { "o", "x" } },
+        textobject_field_outer = { "af", mode = { "o", "x" } },
+        -- Excel-like navigation:
+        -- Use <Tab> and <S-Tab> to move horizontally between fields.
+        -- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
+        -- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
+        jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+        jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+        jump_next_row = { "<Enter>", mode = { "n", "v" } },
+        jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+      },
+    },
+    cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+    init = function()
+      local ok, legendary = pcall(require, "legendary")
+      if not ok then
+        return
+      end
+      local csvview = require("csvview")
+      return legendary.commands({
+        {
+          ":Csv",
+          function()
+            csvview.toggle(0, {
+              parser = {
+                delimiter = ",",
+                quote_char = '"',
+                comment_char = "#",
+              },
+              view = {
+                display_mode = "border",
+                header_lnum = 1,
+              },
+            })
+          end,
+          description = "Toggle CSV view",
+        },
+      })
+    end,
+  },
 }
