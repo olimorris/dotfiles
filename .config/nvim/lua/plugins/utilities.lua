7a78f080c9de70345b624cd1cfcb18993cbe91ba
Oli Morris <olimorris@users.noreply.github.com>
2025-06-03 23:10:42 +0100

    refactor(neovim): thank you `legendary.nvim` for the memories

--- .config/nvim/lua/plugins/utilities.lua
+++ .config/nvim/lua/plugins/utilities.lua
@@ -1,34 +1,6 @@
 return {
   "nvim-lua/plenary.nvim", -- Required dependency for many plugins. Super useful Lua functions
   {
-    "mrjones2014/legendary.nvim", -- A command palette for keymaps, commands and autocmds
-    priority = 10000,
-    lazy = false,
-    dependencies = "kkharji/sqlite.lua",
-    keys = {
-      {
-        "<C-p>",
-        function()
-          require("legendary").find()
-        end,
-        desc = "Open Legendary",
-      },
-    },
-    config = function()
-      require("legendary").setup({
-        -- select_prompt = "Legendary",
-        include_builtin = false,
-        extensions = {
-          codecompanion = false,
-          lazy_nvim = true,
-          which_key = false,
-        },
-        -- Load these with the plugin to ensure they are loaded before any Neovim events
-        autocmds = require("config.autocmds"),
-      })
-    end,
-  },
-  {
     "olimorris/persisted.nvim", -- Session management
     event = "BufReadPre",
     opts = {
