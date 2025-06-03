0a564fc48e6f002ec43d4ae9a097f45e04ad2fe5
Oli Morris <olimorris@users.noreply.github.com>
2025-06-03 22:54:50 +0100

    refactor(neovim): move commands away from legendary.nvim

--- .config/nvim/lua/config/commands.lua
+++ .config/nvim/lua/config/commands.lua
@@ -1,128 +1,76 @@
-local ok, legendary = pcall(require, "legendary")
-if not ok then
-  return
-end
-
-return legendary.commands({
+local commands = {
   {
-    ":LineNumbers",
+    "LineNumbers",
     function()
       om.ToggleLineNumbers()
     end,
-    description = "Toggle line numbers",
+    desc = "Toggle line numbers",
   },
   {
-    ":ChangeFiletype",
+    "ChangeFiletype",
     function()
       om.ChangeFiletype()
     end,
-    description = "Change filetype of current buffer",
+    desc = "Change filetype of current buffer",
   },
   {
-    ":CopyMessage",
+    "CopyMessage",
     function()
       vim.cmd([[let @+ = execute('messages')]])
     end,
-    description = "Copy message output",
+    desc = "Copy message output",
   },
   {
-    ":copen",
-    description = "Open quickfix window",
+    "FindAndReplace",
+    function(opts)
+      vim.api.nvim_command(string.format("silent cdo s/%s/%s", opts.fargs[1], opts.fargs[2]))
+      vim.api.nvim_command("silent cfdo update")
+    end,
+    desc = "Find and Replace (after quickfix)",
+    opts = { nargs = "*" },
   },
   {
-    ":cclose",
-    description = "Close quickfix window",
-  },
-  {
-    itemgroup = "Find and Replace (Global)",
-    icon = "",
-    description = "Find and replace across the project",
-    commands = {
-      {
-        "FindAndReplace",
-        function(opts)
-          vim.api.nvim_command(string.format("silent cdo s/%s/%s", opts.fargs[1], opts.fargs[2]))
-          vim.api.nvim_command("silent cfdo update")
-        end,
-        description = "Find and Replace (after quickfix)",
-        unfinished = true,
-        opts = { nargs = "*" },
-      },
-      {
-        "FindAndReplaceUndo",
-        function(opts)
-          vim.api.nvim_command("silent cdo undo")
-        end,
-        description = "Undo Find and Replace",
-      },
-    },
+    "FindAndReplaceUndo",
+    function(opts)
+      vim.api.nvim_command("silent cdo undo")
+    end,
+    desc = "Undo Find and Replace",
   },
   {
     "GitBranchList",
     function()
       om.ListBranches()
     end,
-    description = "List the Git branches in this repo",
+    desc = "List the Git branches in this repo",
   },
   {
     "GitRemoteSync",
     function()
       om.GitRemoteSync()
     end,
-    description = "Git sync remote repo",
+    desc = "Git sync remote repo",
   },
   {
-    itemgroup = "Lazy.nvim",
-    icon = "",
-    description = "Commands for the Lazy package manager",
-    commands = {
-      {
-        ":Lazy",
-        description = "Open Lazy",
-      },
-      {
-        ":Lazy sync",
-        description = "Install, clean and update",
-      },
-      {
-        ":Lazy clean",
-        description = "Clean",
-      },
-      {
-        ":Lazy restore",
-        description = "Restores plugins to the state in the lockfile",
-      },
-      {
-        ":Lazy profile",
-        description = "Profile",
-      },
-      {
-        ":Lazy log",
-        description = "Log",
-      },
-    },
-  },
-  {
-    ":New",
+    "New",
     ":enew",
-    description = "New buffer",
+    desc = "New buffer",
   },
   {
-    ":Snippets",
+    "Snippets",
     function()
       om.EditSnippet()
     end,
-    description = "Edit Snippets",
+    desc = "Edit Snippets",
   },
   {
-    ":Theme",
+    "Theme",
     function()
       om.ToggleTheme()
     end,
-    description = "Toggle theme",
+    desc = "Toggle theme",
   },
   {
-    ":Uuid",
+    "Uuid",
     function()
       local uuid = vim.fn.system("uuidgen"):gsub("\n", ""):lower()
       local line = vim.fn.getline(".")
@@ -130,6 +78,10 @@ return legendary.commands({
         vim.fn.setline(".", vim.fn.strpart(line, 0, vim.fn.col(".")) .. uuid .. vim.fn.strpart(line, vim.fn.col(".")))
       end)
     end,
-    description = "Generate a UUID and insert it into the buffer",
+    desc = "Generate a UUID and insert it into the buffer",
   },
-})
+}
+
+for _, cmd in ipairs(commands) do
+  om.create_user_command(cmd[1], cmd[2], cmd.desc, cmd.opts)
+end
--- .config/nvim/lua/config/util.lua
+++ .config/nvim/lua/config/util.lua
@@ -16,3 +16,16 @@ om.on_personal = vim.fn.getenv("USER") == "Oli"
 function om.on_big_screen()
   return vim.o.columns > 150 and vim.o.lines >= 40
 end
+
+---Create a user command in Neovim
+---@param name string The name of the command
+---@param command string|function The command to execute, can be a string or a function
+---@param desc string A description of the command
+---@param opts table Optional parameters for the command, such as nargs
+---@return nil
+function om.create_user_command(name, command, desc, opts)
+  vim.api.nvim_create_user_command(name, command, {
+    desc = desc,
+    nargs = opts and opts.nargs or 0,
+  })
+end
