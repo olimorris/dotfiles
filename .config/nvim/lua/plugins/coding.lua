2c67a6004436bf451a635ce3db811cb0b50364f9
Oli Morris <olimorris@users.noreply.github.com>
2025-07-12 13:25:59 +0100

    refactor(neovim): update CodeCompanion config

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -4,66 +4,16 @@ return {
     cmd = { "CodeCompanion", "CodeCompanionChat", "CodeCompanionActions" },
     dependencies = {
       "j-hui/fidget.nvim", -- Display status
-      "ravitemer/codecompanion-history.nvim", -- Save and load conversation history
-      {
-        "ravitemer/mcphub.nvim", -- Manage MCP servers
-        cmd = "MCPHub",
-        build = "npm install -g mcp-hub@latest",
-        config = true,
-      },
       {
         "Davidyz/VectorCode", -- Index and search code in your repositories
         version = "*",
         build = "pipx upgrade vectorcode",
         dependencies = { "nvim-lua/plenary.nvim" },
       },
-      {
-        "HakonHarnes/img-clip.nvim", -- Share images with the chat buffer
-        event = "VeryLazy",
-        cmd = "PasteImage",
-        opts = {
-          filetypes = {
-            codecompanion = {
-              prompt_for_file_name = false,
-              template = "[Image]($FILE_PATH)",
-              use_absolute_path = true,
-            },
-          },
-        },
-      },
       -- { "echasnovski/mini.pick", config = true },
       -- { "ibhagwan/fzf-lua", config = true },
     },
     opts = {
-      extensions = {
-        history = {
-          enabled = true,
-          opts = {
-            keymap = "gh",
-            save_chat_keymap = "sc",
-            auto_save = false,
-            auto_generate_title = true,
-            continue_last_chat = false,
-            delete_on_clearing_chat = false,
-            picker = "snacks",
-            enable_logging = false,
-            dir_to_save = vim.fn.stdpath("data") .. "/codecompanion-history",
-          },
-        },
-        mcphub = {
-          callback = "mcphub.extensions.codecompanion",
-          opts = {
-            make_vars = true,
-            make_slash_commands = true,
-            show_result_in_chat = true,
-          },
-        },
-        vectorcode = {
-          opts = {
-            add_tool = true,
-          },
-        },
-      },
       adapters = {
         anthropic = function()
           return require("codecompanion.adapters").extend("anthropic", {
@@ -86,6 +36,7 @@ return {
             },
           })
         end,
+            env = {
         mistral = function()
           return require("codecompanion.adapters").extend("mistral", {
             env = {
